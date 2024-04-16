feace8b829aefd535b410a7b84e888a5d3789456
EliteRanger1229 <147272797+EliteRanger1229@users.noreply.github.com>
2024-04-16 16:15:15 -0700

    update_task.c
    Saving to keep notes

--- backend/update_task.c
+++ backend/update_task.c
@@ -1,2 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "structure.h"
+
+int update_task(Node *head, char *date, char *time, char *taskDescription)
+{
+  //list nodes, ask user which to update
+  
+  //list variables
+
+  //list date
+  //ask do you want to update date
+    //Have user enter new date
+
+  //list time
+  //ask do you want to update time
+    //Have user enter new time
+
+  //list description
+  //ask do you want to update description
+  
+}
+
 // this section works on updating task on the  todo list
 // please read the documentation section for more information
