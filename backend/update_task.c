7a0bf85e65315b682aaa5514a70f04371ea0a97f
EliteRanger1229 <147272797+EliteRanger1229@users.noreply.github.com>
2024-04-17 10:21:25 -0700

    Update update_task.c
    No errors when compiled in Visual Studio. Should be good unless I missed something. Good start but can probably be streamlined better.

--- backend/update_task.c
+++ backend/update_task.c
@@ -1,24 +1,86 @@
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "structure.h"
 
-int update_task(Node *head, char *date, char *time, char *taskDescription)
+//Function to print all tasks in link list
+void printTasks(struct Node* head, char *date, char *time, char *taskTitle, char *taskDescription)
 {
-  //list nodes, ask user which to update
-  
-  //list variables
+    int counter = 1;
+    struct Node* current = head;
 
-  //list date
-  //ask do you want to update date
-    //Have user enter new date
+    //Loop to print each node in link list
+    while(current != NULL)
+    {
+        printf("%d: %s, %s\n", counter, date, time);    //Prints the number of the node, date and time
+        puts(taskTitle);    //Prints the title of the task
+        printf("\n");
+        puts(taskDescription);  //Prints the descripton of the task
+        printf("\n\n");
 
-  //list time
-  //ask do you want to update time
-    //Have user enter new time
+        current = current->next;
+        counter++;
+    }
+}
 
-  //list description
-  //ask do you want to update description
-  
+//Main function to update task
+void update_task(struct Node* head, char *date, char *time, char *taskTitle, char *taskDescription)
+{
+    int choice = 0, updateDate = 0, updateTime = 0, updateTitle = 0, updateDescription = 0;
+    struct Node* current = head;
+
+    //Asks user for which node they desire to update
+    printTasks(head, date, time, taskTitle, taskDescription);
+    printf("Please enter the corresponding number for the task you would like to update: ");
+    scanf("%d", &choice);
+
+    //Navigates to the desired node
+    for(int i = 1; i < choice; i++)
+    {
+        current = current->next;
+    }
+
+    //Prints the information of the selected task
+    printf("Date: %s\nTime: %s\nTitle: ", current->date, current->time);
+    puts(current->taskTitle);
+    printf("\nDescription: ");
+    puts(current->taskDescription);
+
+    //Asks if the user wants to update the date
+    printf("\n\nDo you want to update the date? (enter 1 for yes or 2 for no)\n");
+    scanf("%d", &updateDate);
+    if(updateDate == 1)
+    {
+        printf("\nPlease enter the new date: ");
+        scanf("%s", current->date);
+    }
+
+    //Asks if the user wants to update the time
+    printf("\n\nDo you want to update the time? (enter 1 for yes or 2 for no)\n");
+    scanf("%d", &updateTime);
+    if(updateDate == 1)
+    {
+        printf("\nPlease enter the new time: ");
+        scanf("%s", current->time);
+    }
+
+    //Asks if the user wants to update the title
+    printf("\n\nDo you want to update the title? (enter 1 for yes or 2 for no)\n");
+    scanf("%d", &updateTitle);
+    if(updateDate == 1)
+    {
+        printf("\nPlease enter the new title: ");
+        scanf("%s", current->taskTitle);
+    }
+
+    //Asks if the user wants to update the description
+    printf("\n\nDo you want to update the description? (enter 1 for yes or 2 for no)\n");
+    scanf("%d", &updateDescription);
+    if(updateDate == 1)
+    {
+        printf("\nPlease enter the new description: ");
+        scanf("%s", current->taskDescription);
+    }
 }
 
 // this section works on updating task on the  todo list
