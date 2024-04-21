31a0c5d2e9a92c4ec5606fdd50c7c4fd30a7ff69
EliteRanger1229 <147272797+EliteRanger1229@users.noreply.github.com>
2024-04-20 19:36:41 -0700

    app.c
    Version 2

--- backend/app.c
+++ backend/app.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "isValid.h"
 #include "structure.h"
 #include "add_task.h"
 #include "read_task.h"
 #include "delete_task.h"
+#include "update_task.h"
 
 // void updateTasks(Node** head); // Assuming this function is defined elsewhere
 int main(int argc, char *argv[]) {
@@ -13,34 +15,37 @@ int main(int argc, char *argv[]) {
     printf("Welcome to the ToDo List Application!\n");
 
     do {
-        printf("Select an option:\n");
+        printf("\nSelect an option:\n");
         printf("0 - Add Task\n");
         printf("1 - Update Task\n");
-        printf("2 - list Tasks \n");
-        printf("5 - Exit\n");
+        printf("2 - List Tasks \n");
+        printf("3 - Delete Task\n");
+        printf("4 - Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 0:
-                getUserInput(&head);  // Handle user input for adding tasks
+                getUserInput(&head);    // Handle user input for adding tasks
                 break;
             case 1:
-                updateTasks(&head);  // Update tasks, assuming function is defined
+                update_task(&head);     // Update tasks, assuming function is defined
                 break;
             case 2:
-                printf("listing tasks. \n");
-                read_tasks(head);      
+                printf("\nlisting tasks. \n");  //Lists tasks
+                read_tasks(head);
+                break;
             case 3:
-                delete_task(&head);
+                delete_task(&head);     //Deletes task
+                break;
             case 4:
-                printf("exiting application\n");
+                printf("exiting application\n"); //Tells the user that the app is exiting
                 break;
              
             default:
-                printf("Invalid choice, please try again.\n");
+                printf("Invalid choice, please try again.\n"); //Lets the user know that they did not enter a listed number (0-4)
         }
-    } while (choice != 2);
+    } while (choice != 4);
 
 
     return 0;  // Exit status of the program
